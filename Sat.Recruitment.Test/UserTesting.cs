d36ba66970ff470141d121ccb2d89d5c4147424b
aleguia <aleguia@outlook.es>
2023-04-27 15:48:11 -0500

    v2

--- Sat.Recruitment.Test/UserTesting.cs
+++ Sat.Recruitment.Test/UserTesting.cs
@@ -1,13 +1,13 @@
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Sat.Recruitment.Api.Application.Commands.UserCommands;
 using Sat.Recruitment.Api.Application.Queries.UserQueries;
 using Sat.Recruitment.Api.Controllers;
-using Sat.Recruitment.Api.Domain.DTOs;
-using Sat.Recruitment.Api.Domain.Models;
+using Sat.Recruitment.Api.Domain.Interfaces;
 using Sat.Recruitment.Api.Infrastructure.DataConfiguration;
 using Sat.Recruitment.Api.Infrastructure.Repositories;
 using Xunit;
@@ -20,29 +20,34 @@ namespace Sat.Recruitment.Test
         private readonly IMediator mediator;
         public UserTesting()
         {
-            string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Files\\{0}", "Users.txt");
-            var files = new FileService(string.Format(path, "Read"), string.Format(path, "Write"));
-            var repository = new UserRepository(files);
-            var validator = new UserCreateCommandValidator();
-
             var servicesType = Type.GetType("Microsoft.Extensions.DependencyInjection.ServiceCollection," +
-                " Microsoft.Extensions.DependencyInjection.Abstractions");
+                 " Microsoft.Extensions.DependencyInjection.Abstractions");
             var services = (IServiceCollection)Activator.CreateInstance(servicesType);
 
-            services.AddTransient<IRequestHandler<UserListQuery, ResultDTO<User>>, UserListQueryHandler>(sp =>
-                new UserListQueryHandler(repository));
+            services.AddTransient<IFileService>(sp =>
+            {
+                string path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Files\\{0}", "Users.txt");
+                return new FileService(string.Format(path, "Read"), string.Format(path, "Write"));
+            });
 
-            services.AddTransient<IRequestHandler<UserCreateCommand, ResultDTO<User>>, UserCreateCommandHandler>(sp =>
-                new UserCreateCommandHandler(repository, validator));
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IValidator<UserCreateCommand>, UserCreateCommandValidator>();
 
-            mediator = new Mediator(services.BuildServiceProvider());
+            services.AddMediatR((config) =>
+            {
+                config.RegisterServicesFromAssembly(typeof(UserCreateCommandHandler).Assembly);
+                config.RegisterServicesFromAssembly(typeof(UserListQueryHandler).Assembly);
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            mediator = serviceProvider.GetRequiredService<IMediator>();
         }
 
         [Fact]
         public async Task CreateUser_Should_Return_Successful_Result()
         {
             //Arange
-            var command = new UserCreateCommand()
+            var commandC = new UserCreateCommand()
             {
                 Name = "Mike",
                 Email = "mike@gmail.com",
@@ -53,12 +58,63 @@ namespace Sat.Recruitment.Test
             };
             var controller = new UsersController(mediator);
 
+            var commandM = new UserCreateCommand()
+            {
+                Name = "Mike M",
+                Email = "mike_M@gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+349 1122354999",
+                UserType = Api.Domain.Enums.UserType.Normal,
+                Money = 132
+            };
+
             //Act
-            var result = await controller.CreateUser(command);
+            var resultController = await controller.CreateUser(commandC); //From Controller
+            var resultMediator = await mediator.Send(commandM); //From Mediator
 
             //Assert
-            Assert.Equal(true, result.IsSuccess);
-            Assert.Equal("User Created", result.Errors);
+            Assert.Equal(true, resultController.IsSuccess);
+            Assert.Equal("User Created", resultController.Errors);
+
+            Assert.Equal(true, resultMediator.IsSuccess);
+            Assert.Equal("User Created", resultMediator.Errors);
+        }
+
+        [Fact]
+        public async Task CreateUser_Should_Return_Error_Result()
+        {
+            //Arange
+            var commandC = new UserCreateCommand()
+            {
+                Name = "Mike S",
+                Email = "mikeS---gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+349 11442454215",
+                UserType = Api.Domain.Enums.UserType.Normal,
+                Money = 124
+            };
+            var controller = new UsersController(mediator);
+
+            var commandM = new UserCreateCommand()
+            {
+                Name = "Mike MS",
+                Email = "mike_M..gmail.com",
+                Address = "Av. Juan G",
+                Phone = "+349 11223549439",
+                UserType = Api.Domain.Enums.UserType.Normal,
+                Money = 132
+            };
+
+            //Act
+            var resultController = await controller.CreateUser(commandC); //From controller
+            var resultMediator = await mediator.Send(commandM); //From Mediator
+
+            //Assert
+            Assert.Equal(false, resultController.IsSuccess);
+            Assert.Equal("Invalid email", resultController.Errors);
+
+            Assert.Equal(false, resultMediator.IsSuccess);
+            Assert.Equal("Invalid email", resultMediator.Errors);
         }
 
         [Fact]
@@ -67,10 +123,14 @@ namespace Sat.Recruitment.Test
             //Arange
             var controller = new UsersController(mediator);
             //Action
-            var result = await controller.ListUser();
+            var resultController = await controller.ListUser(); // From Controller
+            var resultMediator = await mediator.Send(new UserListQuery()); // From Mediator
             //Assert
-            Assert.Equal(result.IsSuccess, true);
-            Assert.Equal(result.Data.Count > 0, true);
+            Assert.Equal(resultController.IsSuccess, true);
+            Assert.Equal(resultController.Data.Count > 0, true);
+
+            Assert.Equal(resultMediator.IsSuccess, true);
+            Assert.Equal(resultMediator.Data.Count > 0, true);
         }
     }
 }
